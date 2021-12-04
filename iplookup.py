a60fca875f32f3f971c08ff0645c057fd3675efe
out2sky <71129559+out2sky@users.noreply.github.com>
2021-12-04 23:20:06 +0800

    Create iplookup.py

---
+++ iplookup.py
@@ -0,0 +1,19 @@
+from qqwry import qqwry
+
+if __name__ == '__main__':
+
+    cz = qqwry()
+    print(cz.get_version())
+
+    infile = open("ip.txt")
+    outfile = open("result.txt",'a')
+
+    while 1:
+        ip = infile.readline()
+        if not ip:
+            break
+        print('{},{}'.format(ip.replace("\n",""),cz.get_addr_by_ip(ip)))
+        outfile.writelines('{},{}\n'.format(ip.replace("\n",""),cz.get_addr_by_ip(ip)))
+
+    infile.close()
+    outfile.close()
